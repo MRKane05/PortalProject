99fe67f4b541494ea2175e5512ef24df74468f91
Mark Kane <markane05@hotmail.com>
2025-06-07 18:48:07 +1200

    Have added elevator models, have reworked textures, and have elevators working in a rough but functional way

---
+++ Portal2018/Assets/GameAssets/Scripts/ActionItems/ElevatorHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This will most likely become an interpolation class that'll also be used for the likes of platforms, but for now lets just get elevators working
+public class ElevatorHandler : MonoBehaviour {
+    public enum enElevatorState { NULL, WAITING, DOORCLOSING, MOVING, DOOROPENING, FINISHED}
+    public enElevatorState ElevatorState = enElevatorState.WAITING;
+
+    public GameObject ElevatorShuttle; //Our bit wot does the moving
+    public GameObject ElevatorBase, ElevatorDestination; //Our two exterior pieces that we'll move between
+
+    public float ElevatorMoveSpeed = 3f;
+
+    public void SetPlayerState(bool bPlayerInVolume)
+    {
+        if (bPlayerInVolume)
+        {
+            PlayerSteppedIntoElevator();
+        }
+    }
+
+    public void PlayerSteppedIntoElevator()
+    {
+        if (ElevatorState == enElevatorState.WAITING)   //Do our door close thing
+        {
+            ElevatorState = enElevatorState.DOORCLOSING;
+        }
+    }
+
+    public void LateUpdate()
+    {
+        switch (ElevatorState)
+        {
+            case enElevatorState.WAITING:
+                break;
+            case enElevatorState.DOORCLOSING:   //Just jump straight to the moving
+                ElevatorState = enElevatorState.MOVING;
+                break;
+            case enElevatorState.MOVING:
+                DoElevatorMove();
+                break;
+            case enElevatorState.DOOROPENING:
+                break;
+            case enElevatorState.FINISHED:
+                break;
+        }
+    }
+
+    void DoElevatorMove()
+    {
+        //What we really need is something that will smoothly transition. What we're going to get for the moment is something that moves!
+        ElevatorShuttle.transform.position = Vector3.MoveTowards(ElevatorShuttle.transform.position, ElevatorDestination.transform.position, ElevatorMoveSpeed*Time.deltaTime);
+    }
+}
---
+++ Portal2018/Assets/GameAssets/Scripts/ActionItems/ElevatorShuttleObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Because the trigger class will be a volume within this space we need to keep an eye on this for handling everything
+public class ElevatorShuttleObject : MonoBehaviour {
+	public ElevatorHandler ourParentHandler;
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			ourParentHandler.SetPlayerState(true);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			ourParentHandler.SetPlayerState(false);
+		}
+	}
+}
