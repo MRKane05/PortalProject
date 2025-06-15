23e2075fd7ea75a5b2e38932ed5ca9625d0d398f
Mark Kane <markane05@hotmail.com>
2025-06-15 18:15:17 +1200

    Cube Dropper modelled, animated, and really just in need of script oversight behavior

---
+++ Portal2018/Assets/GameAssets/Props/Scripts/DropperBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//A basic behavior method for our cube dropper. This'll also have to include spawning/respawning a cube after it's been destroyed. For the moment lets start simple
+public class DropperBehaviour : MonoBehaviour {
+    public List<Animation> ArmAnimations = new List<Animation>();
+
+    public void triggerAnimation(bool bState)
+    {
+        if (bState)
+        {
+            for (int i=0; i<ArmAnimations.Count; i++)
+            {
+                ArmAnimations[i].Play();
+            }
+        }
+    }
+}
