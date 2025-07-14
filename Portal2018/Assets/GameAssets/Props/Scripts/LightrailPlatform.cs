0f62c84396fb9195e4920459f16b010d0ee2d15b
Mark Kane <markane05@hotmail.com>
2025-07-14 21:38:37 +1200

    Have lightrail platform moving and laser texture

---
+++ Portal2018/Assets/GameAssets/Props/Scripts/LightrailPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightrailPlatform : MonoBehaviour {
+	//So we could have the system assign the length of the laser beam
+	public List<GameObject> railPoints;
+
+	public enum enLightrailType { NULL, PINGPONG, RAIL }
+	public enLightrailType LightrailType = enLightrailType.PINGPONG;
+	public GameObject LiftObject;
+	public bool bPlatformActive = false;
+	int platformTarget = 0; //Which point are we currently moving towards?
+	public float platformSpeed = 1.5f;
+	public float endPauseTime = 3f;
+	float pauseTime = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > pauseTime + endPauseTime)
+		{
+			LiftObject.transform.position = Vector3.MoveTowards(LiftObject.transform.position, railPoints[platformTarget].transform.position, platformSpeed * Time.deltaTime);
+		}
+		if (Mathf.Approximately(Vector3.SqrMagnitude(railPoints[platformTarget].transform.position - LiftObject.transform.position), 0f))
+        {
+			platformTarget++;
+			platformTarget = (int)Mathf.Repeat(platformTarget, railPoints.Count);
+			pauseTime = Time.time;
+        }
+	}
+}
